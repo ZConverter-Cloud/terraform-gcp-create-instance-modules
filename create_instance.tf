370a6029f06ee0f26d74c75a6c0f309f5eb470c6
ZConverter-samples <85214181+ZConverter-samples@users.noreply.github.com>
2023-03-04 20:08:11 +0900

    Update create_instance.tf

--- create_instance.tf
+++ create_instance.tf
@@ -1,3 +1,19 @@
+locals {
+  metadata_startup_script = var.OS_name != "windows" && var.user_data_file_path != null && var.user_data_file_path != "null" ? templatefile(
+    "${path.module}/startup_script_sh.tftpl",
+    {
+      userdata = var.OS_name != "windows" ? fileexists(var.user_data_file_path) != false ? file(var.user_data_file_path) : null : null
+    }
+  ) : null
+
+  windows_startup_script_ps1 = var.OS_name == "windows" && var.user_data_file_path != null && var.user_data_file_path != "null" ? templatefile(
+    "${path.module}/startup_script_ps1.tftpl",
+    {
+      userdata = var.OS_name == "windows" ? fileexists(var.user_data_file_path) != false ? file(var.user_data_file_path) : null : null
+    }
+  ) : null
+}
+
 resource "google_compute_instance" "create_gcp_instance" {
   name         = var.vm_name
   machine_type = var.machine_type
@@ -12,20 +28,9 @@ resource "google_compute_instance" "create_gcp_instance" {
   }
 
   tags = ["${var.vm_name}-tag"]
-  metadata_startup_script = var.OS_name != "windows" && var.user_data_file_path != null ? templatefile(
-    "${path.module}/startup_script_sh.tftpl",
-    {
-      userdata = var.OS_name != "windows" ? fileexists(var.user_data_file_path) != false ? file(var.user_data_file_path) : null : null
-    }
-  ) : null
-  
+  metadata_startup_script = local.metadata_startup_script
   metadata = {
-    windows-startup-script-ps1 = var.OS_name == "windows" && var.user_data_file_path != null ? templatefile(
-      "${path.module}/startup_script_ps1.tftpl",
-      {
-        userdata = var.OS_name == "windows" ? fileexists(var.user_data_file_path) != false ? file(var.user_data_file_path) : null : null
-      }
-    ) : null
+    windows-startup-script-ps1 = local.windows_startup_script_ps1
     ssh-keys = var.ssh_public_key != null ? "${var.user_name != null ? var.user_name : var.vm_name}:${var.ssh_public_key}" : null
     user-data = var.OS_name != "windows" ? fileexists(var.user_data_file_path) != false ? file(var.user_data_file_path) : null : null
   }
